89fa5a82f238c771665001d7432b57c04fb1b6f9
NitishVashisht <31930994+NitishVashisht@users.noreply.github.com>
2017-11-02 20:54:32 +0530

    js file regarding what are getter setter

--- properties.js
+++ properties.js
@@ -1,25 +1,30 @@
 "use strict";
 // imoplementation of properties which acts as fileds outside but as methods inside with underscore.
 exports.__esModule = true;
-var property = (function () {
-    function property(_x, _y) {
+var propertee = (function () {
+    function propertee(_x, _y) {
         var _this = this;
         this._x = _x;
         this._y = _y;
         this.didi = function () {
-            // property in intellisense.
+            //taken as property in intellisense. 
             console.log('X: ' + _this._x, 'Y: ' + _this._y);
         };
     }
-    Object.defineProperty(property.prototype, "x", {
-        get: function () { return this._x; },
+    propertee.prototype.display = function () {
+        console.log('X: ' + this._x, 'Y: ' + this._y);
+    };
+    Object.defineProperty(propertee.prototype, "x", {
+        get: function () { return this._x; } // with the use of get and set x and y still will be shown as properties
+        ,
+        //BUT THEY WILL ACT AS METHODS THIS IS BEAUTY OF ANGULAR
         set: function (value) {
             this._x = value;
         },
         enumerable: true,
         configurable: true
     });
-    Object.defineProperty(property.prototype, "y", {
+    Object.defineProperty(propertee.prototype, "y", {
         get: function () { return this._y; },
         set: function (value) {
             this._y = value;
@@ -27,11 +32,17 @@ var property = (function () {
         enumerable: true,
         configurable: true
     });
-    return property;
+    return propertee;
 }());
-exports.property = property;
-var propobject = new property(10, 15); // we can only pass values to constructor in order to update the values of fields
+exports.propertee = propertee;
+var propobject = new propertee(10, 15); // we can only pass values to constructor in order to update the values of fields
 //we cannot even use validations by this means so in order to pass required values to
 //fields we can use concept of getter and setters like in other languages in type script
-//we can bind them, properties also we can use validations in setter functionality.
+//we can bind them, properties also ,we can use validations in setter functionality.
 propobject.didi();
+propobject.x = 500; //because set x() is a property so we cannot call them as methods we can assign valuse
+console.log(propobject.x);
+propobject.didi(); // again to check if value of _x has changed or not.
+var mybad = function (point) { return point.xx + point.yy; };
+var my_bad = mybad({ xx: 10, yy: 20 });
+console.log(my_bad);
