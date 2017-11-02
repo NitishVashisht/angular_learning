f725d8dd0dba9cff0f98dbcecaf48dc898134049
NitishVashisht <31930994+NitishVashisht@users.noreply.github.com>
2017-11-02 20:50:49 +0530

    what are getter setter properties !!!

--- properties.ts
+++ properties.ts
@@ -1,30 +1,50 @@
 // imoplementation of properties which acts as fileds outside but as methods inside with underscore.
 
-export class property{ // access modifiers in constructor .
-
-        constructor(private _x? : number,private _y? : number){}
-        didi =()=>{ //  it is not a method as it is an anoymous declaration of method it will be taken as
-                    // property in intellisense.
-                    
-            console.log('X: ' +this._x,'Y: '+this._y);
-        }  
+export interface my_interface{
+    xx:number;          //we can use semicolon or comma for this functionality in interface.
+    yy:number;
+}
+export class propertee{ // access modifiers in constructor .
     
-    get x(){return this._x ;}
+            constructor(private _x? : number,private _y? : number){}
+            display(){
 
-    set x(value){
-        this._x = value;
-    }
+                console.log('X: ' +this._x,'Y: '+this._y);
+            }
+            didi =()=>{ //  it is not a method as it is an anoymous declaration of method it will be                        
+                        //taken as property in intellisense. 
 
-    get y(){return this._y ;}
+            console.log('X: ' +this._x,'Y: '+this._y);
+            }  
+        
+        get x(){return this._x ;} // with the use of get and set x and y still will be shown as properties
+                                 //BUT THEY WILL ACT AS METHODS THIS IS BEAUTY OF ANGULAR
+    
+        set x(value){
+            this._x = value;
+        }
+    
+        get y(){return this._y ;}
+    
+        set y(value){
+    
+            this._y = value;
+        }
+    }  
+    
+    let propobject = new propertee(10,15); // we can only pass values to constructor in order to update the values of fields
+                                          //we cannot even use validations by this means so in order to pass required values to
+                                          //fields we can use concept of getter and setters like in other languages in type script
+                                         //we can bind them, properties also ,we can use validations in setter functionality.
+    propobject.didi();
 
-    set y(value){
+    propobject.x = 500; //because set x() is a property so we cannot call them as methods we can assign valuse
+    console.log(propobject.x);
 
-        this._y = value;
-    }
-}  
+    propobject.didi(); // again to check if value of _x has changed or not.
 
-let propobject = new property(10,15); // we can only pass values to constructor in order to update the values of fields
-                                      //we cannot even use validations by this means so in order to pass required values to
-                                      //fields we can use concept of getter and setters like in other languages in type script
-                                     //we can bind them, properties also we can use validations in setter functionality.
-propobject.didi();
+    let mybad=(point:my_interface)=>{return point.xx+point.yy ;}
+
+    let my_bad = mybad({xx:10,yy:20});
+
+    console.log(my_bad);
